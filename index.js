fc3166a181dca30636e3e6b1302a4bad5d1a3c4c
peterdemartini <thepeterdemartini@gmail.com>
2018-05-23 14:33:42 -0700

    teraslice-stream now takes an array

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const StreamEntity = require('./lib/stream_entity');
 module.exports = Stream;
 module.exports.StreamEntity = StreamEntity;
 module.exports.Stream = Stream;
-
+module.exports.isStream = input => input instanceof Stream;
+module.exports.isStreamEntity = input => input instanceof StreamEntity;
